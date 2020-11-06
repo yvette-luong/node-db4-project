9386aa456f86f0d68593e3e2c143cf9e66746ea3
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-05 17:00:26 -0800

    update migartions

--- data/migrations/20201105161529_foodrecipe.js
+++ data/migrations/20201105161529_foodrecipe.js
@@ -1,20 +1,36 @@
 
-exports.up = function(knex) {
+
+exports.up = function(knex, Promise) {
     return knex.schema
-      .createTable('', tbl => {
-      })
-      .createTable('', tbl => {
-      })
-      .createTable('', tbl => {
-      })
-      .createTable('', tbl => {
-      })
-  };
-  exports.down = function(knex, Promise) {
-    // drop in the opposite order
-    return knex.schema
-      .dropTableIfExists('')
-      .dropTableIfExists('')
-      .dropTableIfExists('')
-      .dropTableIfExists('')
-  };
+    .createTable('recipes', tbl => {
+        tbl.increments('recipe_id');
+        tbl.string('recipe_name').notNullable().unique();
+        tbl.string('ingredient_name').notNullable().unique().references('ingredient_name').inTable('ingredients');
+
+    })  
+    .createTable('instructions', tbl => {
+        tbl.increments('step_id');
+        tbl.integer('step_order').notNullable();
+        tbl.string('instructions').notNullable();
+        tbl.integer('recipe_id').unsigned().notNullable().references('recipe_id').inTable('recipes');
+    })
+    .createTable('quantity', tbl => {
+        tbl.increments('quantity_id');
+        tbl.float('quantity').notNullable();
+        tbl.integer('recipe_id').unsigned().notNullable().references('recipe_id').inTable('recipes');
+        tbl.integer('ingredient_id').unsigned().notNullable().references('ingredient_id').inTable('ingredients');
+    })
+    .createTable('ingredients', tbl => {
+        tbl.increments('ingredient_id');
+        tbl.string('ingredient_name').notNullable().unique();
+    })
+    };
+    
+    exports.down = function(knex) {
+         // drop in the opposite order
+      return knex.schema
+      .dropTableIfExists('ingredients')
+      .dropTableIfExists('quantity')
+      .dropTableIfExists('instructions')
+      .dropTableIfExists('recipes')
+    };
